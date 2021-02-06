51c1c7705eb3cabd81d358dcf34053fa54b2f3c1
Lahloug <ghali-lahlou998@hotmail.com>
2021-02-06 20:59:30 +0100

    read.py [WIP] reading files

---
+++ servier/read.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+"""
+Main file for reading raw data
+"""
+
+# import from standart library
+import pandas as pd
+from os.path import join
+
+# project import
+from servier.data import DATA_SOURCE
+
+def read_drugs():
+    filename = join(DATA_SOURCE, 'drugs.csv')
+    df = pd.read_csv(filename)
+    print(df)
+    return df
