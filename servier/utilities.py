49a66c13b7e9b8f1b6bf890be1b1abf41e521834
Lahloug <ghali-lahlou998@hotmail.com>
2021-02-07 15:33:09 +0100

    utilities.py [ADD] remove_escaped_characters function

---
+++ servier/utilities.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+
+"""
+Main file for utilities function
+"""
+
+# import from standard library
+from typing import Union
+import pandas as pd
+
+
+def save_csv(df: pd.DataFrame, output_name: str) -> None:
+    filename = join(DATA_SOURCE, output_name)
+    df = pd.to_csv(output_name, index=False)
+
+
+def remove_special_character(s: Union[str, float]) -> Union[str, float]:
+    """
+    Return a string removing certain escaped characters
+    params
+    :s: str
+    return params
+    :s: str
+    """
+    # Return nan if nan
+    if s != s:
+        return s
+    chars_to_remove = ['\\xc3', '\\x28', '\\xb1']
+    repls = tuple((char, "") for char in chars_to_remove)
+    s = reduce(lambda a, kv: a.replace(*kv), repls, s)
+    return s
+
