b778ea3e3d74c5291f7c75962d08d6242a13faaa
Lahloug <ghali-lahlou998@hotmail.com>
2021-02-09 16:53:50 +0100

    read.py [ADD] documentation on yaml use

--- servier/read.py
+++ servier/read.py
@@ -7,7 +7,7 @@ Main file for reading raw data
 # import from standard library
 import pandas as pd
 from os.path import join
-import json
+import yaml
 
 # project import
 from servier.data import DATA_SOURCE
@@ -25,8 +25,12 @@ def read_csv(filename: str) -> pd.DataFrame:
 def read_yaml(filename: str) -> pd.DataFrame:
     """
     Read json file located in the data folder
+    We used yaml library to read it because the file contains
+    an extra comma which is not standard to json.
+    Reading it as a yaml is a neat trick around that.
     """
     filename = join(DATA_SOURCE, filename)
+    # https://stackoverflow.com/questions/52636846/python-cant-parse-json-with-extra-trailing-comma
     with open(filename) as f:
         pubmed = yaml.load(f)
     return pubmed
