b0309afa7f9ebaeef60e279220ce81089f800b67
Lahloug <ghali-lahlou998@hotmail.com>
2021-02-10 12:02:32 +0100

    process.py [ADD] drug_mention for searching words

--- servier/process.py
+++ servier/process.py
@@ -44,9 +44,9 @@ class Trie(object):
         """
         self.root = TrieNode("")
 
-    def insert(self, word: str, journal: str, date: str) -> None:
+    def insert(self, word: str, id: Union(str, int), journal: str, date: str) -> None:
         """Insert a word into the trie"""
-        publication = (journal, date)
+        publication = (id, journal, date)
         node = self.root
 
         # Loop through each character in the word
@@ -93,7 +93,60 @@ def inject_articles(articles: Union[numpy.recarray, list, tuple])) -> Trie:
     Return a Trie object where word are ready to be queried.
     """
     trie = Trie()
-    for journal, title, date in articles:
+    for id, journal, title, date in articles:
         for word in title.split():
-            trie.insert(word, journal, date)
+            trie.insert(word, id, journal, date)
     return trie
+
+
+def drug_mentions(df: pd.DataFrame, publication_type) -> None:
+    """
+    Compute drugs search on journals either on
+    medical publication or clinical trials
+
+    params:
+
+    df:pd.DataFrame: Must contain 4 columns with this exact
+                     name (journal, id, title, date)
+    publication_type:str: either 'pubmed' or 'clinical_trial'
+    schema of final json looks like:
+
+    TODO
+    {'xxx'}
+    """
+    if publication_type == 'pubmed':
+        df = read_csv('pubmed_cleaned.csv')
+    elif publication_type == 'clinical_trial':
+        df = read_csv('clinical_trials.csv')
+    drugs = read_csv('drugs_cleaned.csv')
+    df = df[['id', 'journal', 'title', 'date']].to_records()
+    trie = inject_articles(df)
+    res = {}
+    for row in drugs.iterrows():
+        atccode = row[1]['atccode']
+        drug = row[1]['drug']
+        mentions = trie.query(drug)
+        for id, journal, date in mentions:
+            if journal not in res:
+                res[journal] = {}
+                res[journal][publication_type] = [(id, atccode, date)]
+            else:
+                res[journal][publication_type].append((id, atccode, date))
+    filename = "{}_mentions.pickle".format(publication_type)
+    save_pickle(res, filename)
+
+
+def drug_mention_pubmed():
+    """
+    Compute drug mention on Medical publication
+    """
+    pubmed = read_csv('pubmed_cleaned.csv')
+    drug_mentions(pubmed, 'pubmed')
+
+
+def drug_mention_clinical_trial():
+    """
+    Compute drug mention on Medical publication
+    """
+    ct = read_csv('clinical_trials_cleaned.csv')
+    drug_mentions(ct, 'clinical_trial')
