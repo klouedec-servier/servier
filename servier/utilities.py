5191be762e6572b63d0f1e469bad17d1061575e1
Lahloug <ghali-lahlou998@hotmail.com>
2021-02-10 11:04:04 +0100

    read.py utilities.py [MOVE] functions from read.py to utilities.py

--- servier/utilities.py
+++ servier/utilities.py
@@ -9,16 +9,56 @@ from functools import reduce
 from os.path import join
 from typing import Union
 import pandas as pd
+import pickle
 import string
+import yaml
 
 # import from project
 
 from servier.data import DATA_SOURCE
 
 
+
+
+def read_csv(filename: str) -> pd.DataFrame:
+    """
+    Read csv file located in the data folder
+    """
+    filename = join(DATA_SOURCE, filename)
+    df = pd.read_csv(filename)
+    return df
+
+
+def read_yaml(filename: str) -> pd.DataFrame:
+    """
+    Read json file located in the data folder
+    We used yaml library to read it because the file contains
+    an extra comma which is not standard to json.
+    Reading it as a yaml is a neat trick around that.
+    """
+    filename = join(DATA_SOURCE, filename)
+    # https://stackoverflow.com/questions/52636846/python-cant-parse-json-with-extra-trailing-comma
+    with open(filename) as f:
+        data = yaml.load(f)
+    return data
+
+
+def read_pickle(filename: str) -> dict:
+    filename = join(DATA_SOURCE, filename)
+    with open(filename, 'rb') as handle:
+        d = pickle.load(handle)
+    return d
+
+
 def save_csv(df: pd.DataFrame, output_name: str) -> None:
     filename = join(DATA_SOURCE, output_name)
     df.to_csv(filename, index=False)
+
+
+def save_pickle(d: dict, filename: str) -> None:
+    filename = join(DATA_SOURCE, filename)
+    with open(filename, 'wb') as handle:
+        pickle.dump(d, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def remove_special_character(s: Union[str, float]) -> Union[str, float]:
@@ -41,4 +81,3 @@ def remove_special_character(s: Union[str, float]) -> Union[str, float]:
 def replace_punctuation(s):
     s = s.translate(str.maketrans(string.punctuation, ' ' * len(string.punctuation)))
     return s
-
